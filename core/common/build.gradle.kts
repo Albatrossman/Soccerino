8175265083508ff797f857cff9b99997c4657a03
Pouya Rezaei <PouyaRezaie7@gmail.com>
2025-10-07 01:03:28 +0330

    feat(common): set up Kotlin library convention and add javax.inject
    Replaces the manual configuration for a Kotlin JVM library with a shared convention plugin.
    
    Adds the `javax.inject` dependency as an API dependency.

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,21 +1,9 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget = JvmTarget.JVM_11
-    }
+    id("convention.kotlin.library")
 }
 
 dependencies {
+    api(libs.javax.inject)
+
     implementation(libs.jetbrains.kotlinx.coroutines.core)
 }
