f22ef2a7bc4bd24b8b8aa4415b82495d93c2ec34
Pouya Rezaei <PouyaRezaie7@gmail.com>
2025-10-07 00:55:56 +0330

    refactor(domain): simplify Team model and remove typed ID
    Removes the `TeamId` value class and simplifies the `Team` data class by removing the `id` and `leagueId` fields. The custom `equals` and `hashCode` implementations based on the ID have also been removed. Unit tests for the old `Team` model are deleted accordingly. The `Team` model is now annotated with `@Serializable`.

--- core/domain/src/main/java/ir/miare/domain/model/Team.kt
+++ core/domain/src/main/java/ir/miare/domain/model/Team.kt
@@ -1,21 +1,9 @@
 package ir.miare.domain.model
 
-import ir.miare.domain.model.id.LeagueId
-import ir.miare.domain.model.id.TeamId
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class Team(
-    val id: TeamId,
     val name: String,
-    val rank: Int,
-    val leagueId: LeagueId
-) {
-
-    override fun equals(other: Any?): Boolean {
-        return other is Team && id == other.id
-    }
-
-    override fun hashCode(): Int {
-        return id.hashCode()
-    }
-
-}
+    val rank: Int
+)
