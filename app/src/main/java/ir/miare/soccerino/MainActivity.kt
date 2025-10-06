495f28631935c67b8260f6c540332f93b2398d90
Pouya Rezaei <PouyaRezaie7@gmail.com>
2025-10-07 01:22:49 +0330

    feat(app): configure Hilt and set up main screen
    Adds the `@AndroidEntryPoint` annotation to `MainActivity` to enable Hilt for dependency injection.
    
    The `setContent` block is updated to use a `Surface` with the background color from `MaterialTheme` and sets `MainScreen` as the root composable. The `AppTheme` import path is also corrected.

--- app/src/main/java/ir/miare/soccerino/MainActivity.kt
+++ app/src/main/java/ir/miare/soccerino/MainActivity.kt
@@ -4,16 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import ir.miare.soccerino.ui.theme.AppTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import dagger.hilt.android.AndroidEntryPoint
+import ir.miare.ui.theme.AppTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AppTheme {
-
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    MainScreen()
+                }
             }
         }
     }
+
 }
