c5e2b11ae1b1794aa5e5a0006bb7d546f79ee142
Pouya Rezaei <PouyaRezaie7@gmail.com>
2025-10-07 00:53:28 +0330

    refactor(domain): embed Team in Player and make models Serializable
    Removes `PlayerTest.kt` and refactors the `Player` data class. The `teamId` field is replaced with an embedded `Team` object. Custom `equals` and `hashCode` implementations are removed in favor of the default data class behavior.
    
    Additionally, `Player` and `PlayerId` are now marked as `@Serializable`.

--- core/domain/src/main/java/ir/miare/domain/model/Player.kt
+++ core/domain/src/main/java/ir/miare/domain/model/Player.kt
@@ -1,22 +1,13 @@
 package ir.miare.domain.model
 
 import ir.miare.domain.model.id.PlayerId
-import ir.miare.domain.model.id.TeamId
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class Player(
     val id: PlayerId,
     val name: String,
     val totalGoals: Int,
+    val team: Team,
     val followed: Boolean = false,
-    val teamId: TeamId
-) {
-
-    override fun equals(other: Any?): Boolean {
-        return other is Player && id == other.id
-    }
-
-    override fun hashCode(): Int {
-        return id.hashCode()
-    }
-
-}
+)
--- core/domain/src/main/java/ir/miare/domain/model/id/PlayerId.kt
+++ core/domain/src/main/java/ir/miare/domain/model/id/PlayerId.kt
@@ -1,6 +1,9 @@
 package ir.miare.domain.model.id
 
+import kotlinx.serialization.Serializable
+
 @JvmInline
+@Serializable
 value class PlayerId(val value: String) {
 
     override fun toString(): String {
