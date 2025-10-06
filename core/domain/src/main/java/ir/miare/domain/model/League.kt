f7643c1b07d1c0d5ce921190bafcb8be9f411538
Pouya Rezaei <PouyaRezaie7@gmail.com>
2025-10-07 00:53:12 +0330

    feat(domain): make League model serializable and remove tests
    Makes the `League` and `LeagueId` models serializable by adding the `@Serializable` annotation. The custom `equals` and `hashCode` implementations in the `League` model have been removed in favor of the data class's default behavior.
    
    Additionally, the corresponding `LeagueTest.kt` unit test file has been deleted.

--- core/domain/src/main/java/ir/miare/domain/model/League.kt
+++ core/domain/src/main/java/ir/miare/domain/model/League.kt
@@ -1,21 +1,13 @@
 package ir.miare.domain.model
 
 import ir.miare.domain.model.id.LeagueId
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class League(
     val id: LeagueId,
     val name: String,
     val country: String,
     val rank: Int,
     val totalMatches: Int
-) {
-
-    override fun equals(other: Any?): Boolean {
-        return other is League && id == other.id
-    }
-
-    override fun hashCode(): Int {
-        return id.hashCode()
-    }
-
-}
+)
